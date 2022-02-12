40675c808c21fbad2bae1b8e3c072c10e468bc5e
Lucy-Karimi <karimiluccy@gmail.com>
2022-02-12 10:19:21 +0300

    initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(){
+   var fullName: String = "Lucy Karimi"
+   var age : Int = 22
+   var phoneNumber : String = "0797916750"
+   var weight : Int = 60
+   var studentIsKenyanCitizen :Boolean = true
+
+   println(fullName)
+   println(age)
+   println(phoneNumber)
+   println(weight)
+   println(studentIsKenyanCitizen)
+
+}
